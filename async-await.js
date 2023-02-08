07d99360c2e98d5e2cfce08892a857bfb81d6d69
Veysel Aydin <weycell.aydin09@gmail.com>
2023-02-08 15:42:45 +0300

    review

--- async-await.js
+++ async-await.js
@@ -8,35 +8,38 @@ const getNews = async function () {
     const res = await fetch(url);
     if (!res.ok) {
       isError = true;
-      //   throw new Error(`üzgünüm ama birşeyler yolunda gitmedi: ${res.status}`);
+      // throw new Error(`Something went wrong: ${res.status} `);
     }
     const data = await res.json();
     // console.log(data.articles);
     renderNews(data.articles);
-  } catch (error) {}
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const renderNews = (news) => {
-  const newList = document.getElementById("news-list");
+  const newsList = document.getElementById("news-list");
   if (isError) {
-    newList.innerHTML += `
-    <h2>Haberler Bulunamadi</h2>
+    newsList.innerHTML += `
+      <h2>News Can not be fetched</h2>
+      <img src="./img/404.png" alt="" />
     `;
+    return;
   }
-
   news.forEach((item) => {
-    const { title, description, urlToImage, url } = item;
-    newList.innerHTML += `
+    const { title, description, urlToImage, url } = item; //! dest
+    newsList.innerHTML += `
     <div class="col-md-6 col-lg-4 col-xl-3">
-<div class="card">
-  <img src="${urlToImage}" class="card-img-top" alt="...">
-  <div class="card-body">
-    <h5 class="card-title">${title}</h5>
-    <p class="card-text">${description}</p>
-    <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
-  </div>
-</div>
-</div>
+      <div class="card">
+        <img src="${urlToImage}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <h5 class="card-title">${title}</h5>
+          <p class="card-text">${description}</p>
+          <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
+        </div>
+      </div>
+    </div>
     `;
   });
 };
